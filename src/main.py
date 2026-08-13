6ad93208b4f4f0ee07a5a6e38ce7dccb42cc933b
Skyler Xiao <skyler.xiao.99@gmail.com>
2026-08-13 17:44:27 -0400

    Stage 1: fetch and cache HTML

--- src/main.py
+++ src/main.py
@@ -1,7 +1,31 @@
 import requests
+from pathlib import Path
 
-url = "https://books.toscrape.com/robots.txt"
-response = requests.get(url)
+cache_file = Path("cache/catalogue-page-1.html")
 
-print(response.status_code)
-print(response.text)
+if cache_file.exists():
+    Path("cache/catalogue-page-1.html")
+    print("read from cache")
+
+else:
+    url = "https://books.toscrape.com/catalogue/page-1.html"
+    headers = {
+        "User-Agent": "FlyRankInternshipA9/1.0/skiller99668/scraper"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, timeout=5)
+
+        print(response.status_code)
+        if response.status_code == 200:
+            Path("cache").mkdir(exist_ok=True)
+            # print(response.text)
+
+            Path("cache/catalogue-page-1.html").write_text(
+                response.text
+            )
+        else:
+            print(f"failed to fetch. error code: {response.status_code}")
+
+    except requests.exceptions.Timeout:
+        print("Timeout (5.0 s)")
