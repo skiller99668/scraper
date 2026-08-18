43e3c9b612e3d9c19aab14164899a1ac37f8c9dc
Skyler Xiao <skyler.xiao.99@gmail.com>
2026-08-18 10:37:58 -0400

    Stage 2: discover three catalogue pages

--- src/main.py
+++ src/main.py
@@ -1,31 +1,63 @@
 import requests
 from pathlib import Path
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import time
 
-cache_file = Path("cache/catalogue-page-1.html")
+base_url = "https://books.toscrape.com/"
+start_url = urljoin(base_url, "catalogue/page-1.html")
 
-if cache_file.exists():
-    Path("cache/catalogue-page-1.html")
-    print("read from cache")
-
-else:
-    url = "https://books.toscrape.com/catalogue/page-1.html"
-    headers = {
+headers = {
         "User-Agent": "FlyRankInternshipA9/1.0/skiller99668/scraper"
     }
 
-    try:
-        response = requests.get(url, headers=headers, timeout=5)
+urls = []
+page_url = start_url
 
-        print(response.status_code)
-        if response.status_code == 200:
-            Path("cache").mkdir(exist_ok=True)
-            # print(response.text)
+pages = 0
+while page_url and pages < 3:
+    # print("processing", page_url)
+    cache_file = Path(f"cache/{page_url.strip("/").split("/")[-1] }")
 
-            Path("cache/catalogue-page-1.html").write_text(
-                response.text
-            )
-        else:
-            print(f"failed to fetch. error code: {response.status_code}")
+    if cache_file.exists():
+        html = cache_file.read_text(encoding="utf-8")
+        # print("read from cache")
+    else:
+        time.sleep(0.5)
+        try:
+            response = requests.get(page_url, headers=headers, timeout=5)
 
-    except requests.exceptions.Timeout:
-        print("Timeout (5.0 s)")
+            # print(response.status_code)
+            if response.status_code == 200:
+                Path("cache").mkdir(exist_ok=True)
+                # print(response.text)
+
+                html = response.text
+                cache_file.write_text(html, encoding="utf-8")
+            else:
+                print(f"failed to fetch. error code: {response.status_code}")
+
+        except requests.exceptions.Timeout:
+            print("Timeout (5.0 s)")
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    for book in soup.find_all("article"):
+        link = book.find("a")
+        # print(link.get("href"))
+        full_url = urljoin(page_url, link.get("href"))
+        urls.append(full_url)
+
+        # see if next page
+    next_page = soup.find("li", class_="next")
+    if next_page:
+        next_href = next_page.find("a")["href"]
+        page_url = urljoin(page_url, next_href)
+    else:
+        page_url = None
+
+    pages+=1
+
+print("catalogue_pages =", pages)
+print("discovered =", len(urls))
+print("unique_urls =", len(set(urls)))
